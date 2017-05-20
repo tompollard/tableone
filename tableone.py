076f4d1e330592ce25a0a35d370fbb51e06eae45
Tom Pollard <tpollard@mit.edu>
2017-05-20 18:29:58 -0400

    update version

--- tableone.py
+++ tableone.py
@@ -4,7 +4,7 @@ inspired by the R package of the same name.
 """
 
 __author__ = "Tom Pollard <tpollard@mit.edu>"
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 import pandas as pd
 from tabulate import tabulate
