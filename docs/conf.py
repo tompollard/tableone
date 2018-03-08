eaeee20a33b52adf83befe66c8da2761fb496f85
Alistair Johnson <alistairewj@gmail.com>
2018-03-08 13:26:52 -0500

    remove unused options

--- docs/conf.py
+++ docs/conf.py
@@ -101,20 +101,16 @@ html_theme_options = {
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+    'navigation_depth': 4
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = [] # ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
