a2cd6ef694c724deb09bd78441edef23e90fb2b5
Tom Pollard <tpollard@mit.edu>
2018-03-19 10:21:20 -0400

    add modality module. ref #19, #22, #28.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.1',
+    version='0.5.2',
 
     description='TableOne',
     long_description=long_description,
@@ -42,7 +42,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=['tableone'],
+    py_modules=['tableone','modality'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
