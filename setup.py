d8facf2b14a23f8b26e7ce94f32346bc1e08cc0a
Tom Pollard <tpollard@mit.edu>
2018-06-01 13:07:46 -0400

    pypi not rendering markdown

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.8',
+    version='0.5.10',
 
     description='TableOne',
     long_description=long_description,
