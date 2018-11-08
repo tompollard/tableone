f65eadcd0ee07e5f8782adcef722bef5d66b80eb
Tom Pollard <tpollard@mit.edu>
2018-11-08 11:24:49 -0500

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.13',
+    version='0.5.14',
 
     description='TableOne',
     long_description=long_description,
