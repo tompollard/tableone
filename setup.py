b48a2bb517ef905a0892e78da1d09f2de444830d
Lucas Bulgarelli <lucas.bulgarelli@endpointhealth.com>
2022-08-16 13:32:21 -0700

    update requirements

--- setup.py
+++ setup.py
@@ -49,11 +49,11 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'numpy>=1.12.1',
-        'pandas>=0.22.0',
-        'scipy>=0.18.1',
-        'statsmodels>=0.8.0',
-        'tabulate>=0.8.2'
+        'numpy>=1.23.2',
+        'pandas>=1.4.3',
+        'scipy>=1.9.0',
+        'statsmodels>=0.13.2',
+        'tabulate>=0.8.10'
         ],
 
     # List additional groups of dependencies here (e.g. development
