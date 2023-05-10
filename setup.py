da613f265677db9c7544d8e5db78038e14a73b5f
Tom Pollard <tpollard@mit.edu>
2023-05-09 22:23:52 -0400

    rm unused imports

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 from pathlib import Path
 
 # To use a consistent encoding
-from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
